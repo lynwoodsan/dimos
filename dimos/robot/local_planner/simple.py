73d82fd8594ac1701faea18593818caaae5df46f
lesh <lesh@sysphere.org>
2025-12-09 22:10:03 +0000

    working simple planner
    Former-commit-id: 0f84665daf1ff286628197a3c1929d63b9866c21 [formerly 4416f23ff440ce9140f3a3a986e6286e5680b05d]
    Former-commit-id: cca5de1e1c37f64718805bc6d3f3e7db47f86860

--- dimos/robot/local_planner/simple.py
+++ dimos/robot/local_planner/simple.py
@@ -45,37 +45,45 @@ from dimos.utils.threadpool import get_scheduler
 logger = setup_logger("dimos.robot.unitree.local_planner")
 
 
-def vector_to_twist(vector: Vector3) -> Twist:
-    """Convert a Vector3 to a Twist message."""
-    twist = Twist()
-    twist.linear.x = vector.x
-    twist.linear.y = vector.y
-    twist.linear.z = 0.0
-    twist.angular.x = 0.0
-    twist.angular.y = 0.0
-    twist.angular.z = vector.z
-    return twist
-
-
 class SimplePlanner(Module):
     path: In[Path] = None
-    movecmd: Out[Twist] = None
-    speed: float = 0.3
+    movecmd: Out[Vector3] = None
+    speed: float = 2.0
 
     tf: TF
+    current_path: Optional[Path] = None
 
     def __init__(self):
         Module.__init__(self)
         self.tf = TF()
 
+    def poppath(self, direction):
+        if direction.position.length() < 0.5:
+            if self.current_path and len(self.current_path) > 1:
+                self.current_path = self.current_path.tail()
+        return direction.position
+
     def move_stream(self, frequency: float = 20.0) -> rx.Observable:
-        return rx.interval(1.0 / frequency, scheduler=get_scheduler()).pipe(
-            ops.filter(lambda _: self.tf.get("world", "base_link") and self.goal),
-            ops.map(lambda base_link: self.goal @ base_link),
-            ops.map(lambda direction: direction.normalize() * self.speed),
+        return rx.interval(1.0 / frequency).pipe(
+            ops.filter(
+                lambda _: self.current_path and len(self.current_path)
+            ),  # do we have a target path?
+            ops.map(lambda _: self.tf.get("base_link", "world")),
+            ops.filter(lambda _: _),  # do we have a transform?
+            ops.map(lambda tf: self.current_path.head() @ tf),
+            ops.map(self.poppath),
+            ops.map(lambda direction_vector: direction_vector.normalize() * self.speed),
         )
+
+    def set_goal(self, path: Path):
+        self.current_path = path
 
     @rpc
     def start(self):
         self.path.subscribe(self.set_goal)
-        self.move_stream(frequency=20.0).subscribe(self.movecmd.publish)
+
+        def pub(v):
+            print(v)
+            self.movecmd.publish(v)
+
+        self.move_stream(frequency=20.0).subscribe(pub)
