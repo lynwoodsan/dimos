829412b95e0781769284ec3b36607921c6e23307
Jeff Hykin <jeff.hykin@gmail.com>
2026-02-07 14:27:40 -0800

    make blueprint imports lazy

--- dimos/robot/unitree/g1/blueprints/__init__.py
+++ dimos/robot/unitree/g1/blueprints/__init__.py
@@ -15,38 +15,23 @@
 
 """Cascaded G1 blueprints split into focused modules."""
 
-from dimos.robot.unitree.g1.blueprints.agentic._agentic_skills import _agentic_skills
-from dimos.robot.unitree.g1.blueprints.agentic.unitree_g1_agentic import unitree_g1_agentic
-from dimos.robot.unitree.g1.blueprints.agentic.unitree_g1_agentic_sim import unitree_g1_agentic_sim
-from dimos.robot.unitree.g1.blueprints.agentic.unitree_g1_full import unitree_g1_full
-from dimos.robot.unitree.g1.blueprints.basic.unitree_g1_basic import unitree_g1_basic
-from dimos.robot.unitree.g1.blueprints.basic.unitree_g1_basic_sim import unitree_g1_basic_sim
-from dimos.robot.unitree.g1.blueprints.basic.unitree_g1_joystick import unitree_g1_joystick
-from dimos.robot.unitree.g1.blueprints.perceptive._perception_and_memory import (
-    _perception_and_memory,
-)
-from dimos.robot.unitree.g1.blueprints.perceptive.unitree_g1 import unitree_g1
-from dimos.robot.unitree.g1.blueprints.perceptive.unitree_g1_detection import unitree_g1_detection
-from dimos.robot.unitree.g1.blueprints.perceptive.unitree_g1_shm import unitree_g1_shm
-from dimos.robot.unitree.g1.blueprints.perceptive.unitree_g1_sim import unitree_g1_sim
-from dimos.robot.unitree.g1.blueprints.primitive.uintree_g1_primitive_no_nav import (
-    uintree_g1_basic_no_nav,
-    uintree_g1_basic_no_nav as basic_no_nav,
-)
+import lazy_loader as lazy
 
-__all__ = [
-    "_agentic_skills",
-    "_perception_and_memory",
-    "basic_no_nav",
-    "uintree_g1_basic_no_nav",
-    "unitree_g1",
-    "unitree_g1_agentic",
-    "unitree_g1_agentic_sim",
-    "unitree_g1_basic",
-    "unitree_g1_basic_sim",
-    "unitree_g1_detection",
-    "unitree_g1_full",
-    "unitree_g1_joystick",
-    "unitree_g1_shm",
-    "unitree_g1_sim",
-]
+__getattr__, __dir__, __all__ = lazy.attach(
+    __name__,
+    submod_attrs={
+        "agentic._agentic_skills": ["_agentic_skills"],
+        "agentic.unitree_g1_agentic": ["unitree_g1_agentic"],
+        "agentic.unitree_g1_agentic_sim": ["unitree_g1_agentic_sim"],
+        "agentic.unitree_g1_full": ["unitree_g1_full"],
+        "basic.unitree_g1_basic": ["unitree_g1_basic"],
+        "basic.unitree_g1_basic_sim": ["unitree_g1_basic_sim"],
+        "basic.unitree_g1_joystick": ["unitree_g1_joystick"],
+        "perceptive._perception_and_memory": ["_perception_and_memory"],
+        "perceptive.unitree_g1": ["unitree_g1"],
+        "perceptive.unitree_g1_detection": ["unitree_g1_detection"],
+        "perceptive.unitree_g1_shm": ["unitree_g1_shm"],
+        "perceptive.unitree_g1_sim": ["unitree_g1_sim"],
+        "primitive.uintree_g1_primitive_no_nav": ["uintree_g1_basic_no_nav", "basic_no_nav"],
+    },
+)
--- dimos/robot/unitree/go2/blueprints/__init__.py
+++ dimos/robot/unitree/go2/blueprints/__init__.py
@@ -15,49 +15,23 @@
 
 """Cascaded GO2 blueprints split into focused modules."""
 
-from dimos.robot.unitree.go2.blueprints.agentic._common_agentic import _common_agentic
-from dimos.robot.unitree.go2.blueprints.agentic.unitree_go2_agentic import unitree_go2_agentic
-from dimos.robot.unitree.go2.blueprints.agentic.unitree_go2_agentic_huggingface import (
-    unitree_go2_agentic_huggingface,
-)
-from dimos.robot.unitree.go2.blueprints.agentic.unitree_go2_agentic_mcp import (
-    unitree_go2_agentic_mcp,
-)
-from dimos.robot.unitree.go2.blueprints.agentic.unitree_go2_agentic_ollama import (
-    unitree_go2_agentic_ollama,
-)
-from dimos.robot.unitree.go2.blueprints.agentic.unitree_go2_temporal_memory import (
-    unitree_go2_temporal_memory,
-)
-from dimos.robot.unitree.go2.blueprints.basic.unitree_go2_basic import (
-    _linux,
-    _mac,
-    unitree_go2_basic,
-)
-from dimos.robot.unitree.go2.blueprints.smart._with_jpeg import _with_jpeglcm, _with_jpegshm
-from dimos.robot.unitree.go2.blueprints.smart.unitree_go2 import unitree_go2
-from dimos.robot.unitree.go2.blueprints.smart.unitree_go2_detection import unitree_go2_detection
-from dimos.robot.unitree.go2.blueprints.smart.unitree_go2_ros import unitree_go2_ros
-from dimos.robot.unitree.go2.blueprints.smart.unitree_go2_spatial import unitree_go2_spatial
-from dimos.robot.unitree.go2.blueprints.smart.unitree_go2_vlm_stream_test import (
-    unitree_go2_vlm_stream_test,
-)
+import lazy_loader as lazy
 
-__all__ = [
-    "_common_agentic",
-    "_linux",
-    "_mac",
-    "_with_jpeglcm",
-    "_with_jpegshm",
-    "unitree_go2",
-    "unitree_go2_agentic",
-    "unitree_go2_agentic_huggingface",
-    "unitree_go2_agentic_mcp",
-    "unitree_go2_agentic_ollama",
-    "unitree_go2_basic",
-    "unitree_go2_detection",
-    "unitree_go2_ros",
-    "unitree_go2_spatial",
-    "unitree_go2_temporal_memory",
-    "unitree_go2_vlm_stream_test",
-]
+__getattr__, __dir__, __all__ = lazy.attach(
+    __name__,
+    submod_attrs={
+        "agentic._common_agentic": ["_common_agentic"],
+        "agentic.unitree_go2_agentic": ["unitree_go2_agentic"],
+        "agentic.unitree_go2_agentic_huggingface": ["unitree_go2_agentic_huggingface"],
+        "agentic.unitree_go2_agentic_mcp": ["unitree_go2_agentic_mcp"],
+        "agentic.unitree_go2_agentic_ollama": ["unitree_go2_agentic_ollama"],
+        "agentic.unitree_go2_temporal_memory": ["unitree_go2_temporal_memory"],
+        "basic.unitree_go2_basic": ["_linux", "_mac", "unitree_go2_basic"],
+        "smart._with_jpeg": ["_with_jpeglcm", "_with_jpegshm"],
+        "smart.unitree_go2": ["unitree_go2"],
+        "smart.unitree_go2_detection": ["unitree_go2_detection"],
+        "smart.unitree_go2_ros": ["unitree_go2_ros"],
+        "smart.unitree_go2_spatial": ["unitree_go2_spatial"],
+        "smart.unitree_go2_vlm_stream_test": ["unitree_go2_vlm_stream_test"],
+    },
+)
